bf4f70629122624a46a03dfab386cd491658e059
Saarn0 <tdallest@gmail.com>
2025-05-11 19:26:01 -0400

    Generate password method done

--- generator.py
+++ generator.py
@@ -2,6 +2,12 @@ import random
 import string
 
 LENGTH = 12
+alphabet = string.ascii_letters + string.digits + string.punctuation
 
 def generate_password():
-    password = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(alphabet) for i in range(LENGTH))
+    return password
+        
+password = generate_password()        
+print(password)
+    
